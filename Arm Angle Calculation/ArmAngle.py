34caeb937a733e7226a4d02f51e7da119fd66072
ThibaudMZN <thibaud.marazano@gmail.com>
2017-09-12 22:02:32 +0200

    Adding comments

--- Arm Angle Calculation/ArmAngle.py
+++ Arm Angle Calculation/ArmAngle.py
@@ -6,76 +6,87 @@ import math
 import numpy as np
 
 class trackerPoint(object):
-   def __init__(self,x,y,size,frame):
+    def __init__(self, x, y, size, frame):
+       # KCF tracker init
        self.tracker = cv2.TrackerKCF_create()
-       self.bbox = (x-size/2,y-size/2,size,size)
-       self.tracker.init(frame,self.bbox)
+       self.bbox = (x-size/2, y-size/2, size,size)
+       self.tracker.init(frame, self.bbox)
        self.x = x
        self.y = y
        self.size = size
        self.ptsize = 4
-   def update(self,frame,frameToDrawOn):
+
+    def update(self, frame, frameToDrawOn):
        ok, self.bbox = self.tracker.update(frame)
        if ok:
+           # Draw the new point
            self.x = int(self.bbox[0] + self.size/2)
            self.y = int(self.bbox[1] + self.size/2)
-           p1 = (self.x-self.ptsize,self.y-self.ptsize)
-           p2 = (self.x+self.ptsize,self.y+self.ptsize)
-           cv2.rectangle(frameToDrawOn, p1, p2, (0,0,255),-1)
+           p1 = (self.x-self.ptsize, self.y-self.ptsize)
+           p2 = (self.x+self.ptsize, self.y+self.ptsize)
+           cv2.rectangle(frameToDrawOn, p1, p2, (0,0,255), -1)
 
-def Dist(p1,p2):
-   x1=p1.x
-   y1=p1.y
-   x2=p2.x
-   y2=p2.y
-   return math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2))
+def Dist(p1, p2):
+   x1 = p1.x
+   y1 = p1.y
+   x2 = p2.x
+   y2 = p2.y
+   return math.sqrt(math.pow((x2-x1), 2)+math.pow((y2-y1), 2))
 
 def calcAngle(pTrack):
-    a = Dist(pTrack[0],pTrack[1])
-    b = Dist(pTrack[1],pTrack[2])
-    c = Dist(pTrack[2],pTrack[0])
+    a = Dist(pTrack[0], pTrack[1])
+    b = Dist(pTrack[1], pTrack[2])
+    c = Dist(pTrack[2], pTrack[0])
     angRad = math.acos(((a*a)+(b*b)-(c*c))/(2*a*b))
     return math.degrees(angRad)
 
-def drawLine(frame,pTrack):
-    cv2.line(frame,(pTrack[0].x,pTrack[0].y),(pTrack[1].x,pTrack[1].y),(255,0,255),2)
-    cv2.line(frame,(pTrack[1].x,pTrack[1].y),(pTrack[2].x,pTrack[2].y),(255,0,255),2)
+def drawLine(frame, pTrack):
+    cv2.line(frame, (pTrack[0].x,pTrack[0].y), (pTrack[1].x,pTrack[1].y), (255,0,255), 2)
+    cv2.line(frame, (pTrack[1].x,pTrack[1].y), (pTrack[2].x,pTrack[2].y), (255,0,255), 2)
 
 def main():
-    kernel = np.ones((3,3),np.uint8)
+    # Init kernel for erode / dilate
+    kernel = np.ones((3,3), np.uint8)
+    # Init media in/out
     cap = cv2.VideoCapture('Video.mp4')
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, 30.0, (1280,720))
+    out = cv2.VideoWriter('output.avi', fourcc, 30.0, (1280,720))
+    # Read first frame for trackers
     ret, frame = cap.read()
+    # Instantiate trackers at known positions
     pList = [(561,421),(656,385),(584,263)]
     pTrack = []
-
     for pt in pList:
-        pTrack.append(trackerPoint(pt[0],pt[1],80,frame))
+        pTrack.append(trackerPoint(pt[0], pt[1], 80, frame))
 
     while(cap.isOpened()):
+        # Read new frame
         ret, frame = cap.read()
         if(frame is None):
             break
-        thresh1 = cv2.inRange(frame,(170,170,170),(255,255,255))
-        thresh1 = cv2.erode(thresh1,kernel,iterations = 3)
-        thresh1 = cv2.dilate(thresh1,kernel,iterations = 3)
-
-        res = cv2.bitwise_and(frame,frame,mask=thresh1)
+        # Thresholde / Erosion / Dilatation for arm detection
+        thresh1 = cv2.inRange(frame, (170,170,170), (255,255,255))
+        thresh1 = cv2.erode(thresh1, kernel, iterations = 3)
+        thresh1 = cv2.dilate(thresh1, kernel, iterations = 3)
+        # Mask
+        res = cv2.bitwise_and(frame, frame, mask=thresh1)
+        # Update trackers
         for p in pTrack:
             p.update(res, frame)
-
-        drawLine(frame,pTrack)
+        drawLine(frame, pTrack)
+        # Calculate angle between points
         ang = calcAngle(pTrack)
         strAng = "%2.2f deg" % ang
-        cv2.putText(frame,strAng,(pTrack[1].x+40,pTrack[1].y),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255))
-
-        cv2.imshow('frame',frame)
+        # Display it
+        cv2.putText(frame, strAng, (pTrack[1].x+40,pTrack[1].y), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255))
+        # Show image
+        cv2.imshow('frame', frame)
+        # Write to output video
         out.write(frame)
-
+        # "q" key to escape
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+    # Release everything
     cap.release()
     out.release()
     cv2.destroyAllWindows()
